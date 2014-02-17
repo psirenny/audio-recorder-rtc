f473a54403a9f60e4f0debb359ffc21d58b89c8f
psirenny <djtorres0@gmail.com>
2014-02-17 12:19:16 -0500

    add send

--- index.js
+++ index.js
@@ -14,15 +14,25 @@ module.exports = function () {
       );
     },
     send: function (url, callback) {
-      callback();
+      var data = new FormData();
+      var req = new XMLHttpRequest();
+      data.append('audio', this.rec.getBlob());
+      req.open('POST', url, true);
+      req.onload = function (e) {
+        var res = JSON.parse(e.target.response);
+        callback(null, res.url);
+      };
+      req.send(data);
     },
     start: function (callback) {
       this.rec.startRecording();
       callback();
     },
     stop: function (callback) {
-      this.rec.stopRecording(function (audioUrl) {
-        callback(null, audioUrl);
+      var self = this;
+      this.rec.stopRecording(function (url) {
+        self.rec.url = url;
+        callback();
       });
     }
   };
