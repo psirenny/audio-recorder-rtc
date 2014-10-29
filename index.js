490a39d9ac064ad96ba574002f44c9c73119d7f0
psirenny <djtorres0@gmail.com>
2014-10-28 23:07:56 -0400

    0.3.0

--- index.js
+++ index.js
@@ -1,3 +1,59 @@
+function Strategy () {}
+
+Strategy.prototype.available = function (callback) {
+  if (typeof navigator === 'undefined') return callback(null, false);
+  callback(null, !!navigator.getUserMedia);
+};
+
+Strategy.prototype.create = function (callback) {
+  this.data = {};
+  callback(null);
+};
+
+Strategy.prototype.destroy = function (callback) {
+  this.data = {};
+  callback(null);
+};
+
+Strategy.prototype.permission = function (callback) {
+  var self = this;
+
+  if (this.data.rec) {
+    return callback(null, true);
+  }
+
+  function success(mediaStream) {
+    self.data.rec = RecordRTC(mediaStream);
+    callback(null, true);
+  }
+
+  function failure() {
+    callback(null, false);
+  }
+
+  navigator.getUserMedia({audio: true}, success, failure);
+};
+
+Strategy.prototype.start = function (callback) {
+  console.log('start');
+  this.data.rec.startRecording();
+  console.log('callback');
+  callback(null);
+};
+
+Strategy.prototype.stop = function (callback) {
+  var self = this;
+  this.data.rec.stopRecording(function (url) {
+    self.data.rec.url = url;
+    callback(null, url);
+  });
+};
+
+module.exports = function () {
+  return new Strategy();
+};
+
+/*
 module.exports = function () {
   return {
     available: function (callback) {
@@ -46,4 +102,4 @@ module.exports = function () {
       });
     }
   };
-};
+};*/
