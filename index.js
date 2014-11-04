3990df781ed3ec98b6619e9e85730f156d469f8b
psirenny <djtorres0@gmail.com>
2014-11-04 11:35:50 -0500

    remove debug/comments, use newer version of audio-recorder

--- index.js
+++ index.js
@@ -35,9 +35,7 @@ Strategy.prototype.permission = function (callback) {
 };
 
 Strategy.prototype.start = function (callback) {
-  console.log('start');
   this.data.rec.startRecording();
-  console.log('callback');
   callback(null);
 };
 
@@ -45,61 +43,10 @@ Strategy.prototype.stop = function (callback) {
   var self = this;
   this.data.rec.stopRecording(function (url) {
     self.data.rec.url = url;
-    callback(null, url);
+    callback(null, {type: 'wav', url: url});
   });
 };
 
 module.exports = function () {
   return new Strategy();
 };
-
-/*
-module.exports = function () {
-  return {
-    available: function (callback) {
-      if (typeof navigator === 'undefined') return callback(false);
-      return callback(!!navigator.getUserMedia);
-    },
-    clear: function (callback) {
-      this.rec.url = null;
-      callback();
-    },
-    name: 'rtc',
-    permission: function (callback) {
-      var self = this;
-      if (this.rec) return callback();
-
-      function success(mediaStream) {
-        self.rec = RecordRTC(mediaStream);
-        callback();
-      }
-
-      navigator.getUserMedia(
-        {audio: true}, success, callback
-      );
-    },
-    send: function (url, callback) {
-      var data = new FormData();
-      var req = new XMLHttpRequest();
-      data.append('audio', this.rec.getBlob());
-      req.open('POST', url, true);
-      req.onload = function (e) {
-        var res = JSON.parse(e.target.response);
-        var success = e.target.status === 200;
-        callback(success ? null : res, res);
-      };
-      req.send(data);
-    },
-    start: function (callback) {
-      this.rec.startRecording();
-      callback();
-    },
-    stop: function (callback) {
-      var self = this;
-      this.rec.stopRecording(function (url) {
-        self.rec.url = url;
-        callback();
-      });
-    }
-  };
-};*/
